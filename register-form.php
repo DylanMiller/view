9c0840949bf71f445c17a4e4326e76b82ac281e9
Dylan Miller <dmiller3199@gmail.com>
2014-12-02 08:47:56 -0800

    video 37

---
+++ register-form.php
@@ -0,0 +1,19 @@
+<form>
+    <div>
+        <label for="email"></label>
+        <input type="type" name="email" />
+    </div>
+    
+    
+     <div>
+         <label for="username"></label>
+         <input type="type" name="username" />
+    </div>
+    <div>
+         <label for=""></label>
+         <input type="password" name="password" />
+    </div>
+    <div>
+        <buton type="submit">Submit</buton>
+    </div>
+</form>
