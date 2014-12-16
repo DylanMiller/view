7c33eac925e05b4d5452432f6cc783abed29e962
Dylan Miller <dmiller3199@gmail.com>
2014-12-16 08:15:47 -0800

    adding bootstrap

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <link href="/../view/blog.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
