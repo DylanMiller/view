d92bbf774f2b360b6f6a99f29a609132fc251514
Dylan Miller <dmiller3199@gmail.com>
2014-12-15 16:56:26 -0800

    vid 51

--- form.php
+++ form.php
@@ -1,5 +1,11 @@
 <?php
         require_once(__DIR__ . "/../model/config.php");
+        require_once(__DIR__ . "/../controller/login-verify.php");
+        
+        if(!authenticateUser()) {
+            header("Login: " . $path . "index.php");
+            die();
+        }
 ?>
 
 <h1>Create blog post</h1>
