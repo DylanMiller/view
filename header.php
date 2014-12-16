aed3e481e3c343cad1eb103eccfc84392ed1b71b
Dylan Miller <dmiller3199@gmail.com>
2014-12-16 15:52:49 -0800

    this is my final push. for some reason my project is totally jacked. i dont know what to do i've searched for answers and nothing is helping. no one can fix my project so here it is, ill still search for a solution but if not, this is my project.

--- header.php
+++ header.php
@@ -3,6 +3,9 @@
     <head>
         <link href="/../view/blog.css" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+         <?php
+            error_reporting(E_ALL ^ E_NOTICE);
+         ?>
         <meta charset="UTF-8">
         <title></title>
     </head>
