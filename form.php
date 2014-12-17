e95aaf7df3a7182d95ceb78eaad3cef3d51e9184
Dylan Miller <dmiller3199@gmail.com>
2014-12-16 21:26:32 -0800

    still fixing things

--- form.php
+++ form.php
@@ -2,10 +2,7 @@
         require_once(__DIR__ . "/../model/config.php");
         require_once(__DIR__ . "/../controller/login-verify.php");
         
-        if(!authenticateUser()) {
-            header("Location: " . $path . "index.php");
-            die();
-        }
+        
 ?>
 
 <h1>Create blog post</h1>
